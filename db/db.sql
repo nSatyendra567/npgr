af96d3b5814736957e8920e849b35ee9f1760158
satyendra <msatyendra567@gmail.com>
2024-07-27 18:13:45 +0530

    done

--- db/db.sql
+++ db/db.sql
@@ -87,5 +87,4 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
 (1, 'Kush Tiwari ', 'test@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef');
 
 INSERT INTO `admin` (`id`, `name`, `password`) VALUES
-(1, 'admin', '6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2'),
-(2, 'rahul', '40bd001563085fc35165329ea1ff5c5ecbdbbeef');
+(1, 'admin', '6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2');
