13ea6cea6d175d20ba5373525213b731e1cb1c0b
satyendra <msatyendra567@gmail.com>
2024-07-27 18:08:55 +0530

    db

---
+++ db/db.sql
@@ -0,0 +1,91 @@
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `id` int NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `advocate`;
+CREATE TABLE `advocate` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) NOT NULL,
+  `Photo` varchar(255) DEFAULT NULL,
+  `Court` varchar(255) DEFAULT NULL,
+  `State` varchar(255) DEFAULT NULL,
+  `officeAdd` varchar(255) DEFAULT NULL,
+  `chamberAdd` varchar(255) DEFAULT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  `Phone` varchar(15) DEFAULT NULL,
+  `EnrollmentId` varchar(50) DEFAULT NULL,
+  `CourtId` varchar(50) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `approved` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `EnrollmentId` (`EnrollmentId`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `case`;
+CREATE TABLE `case` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Phone` varchar(15) NOT NULL,
+  `Subject` varchar(255) NOT NULL,
+  `Complain_File` varchar(255) DEFAULT NULL,
+  `Description` text NOT NULL,
+  `Status` varchar(20) DEFAULT NULL,
+  `Urgent` tinyint(1) NOT NULL DEFAULT '0',
+  `ReferenceNo` varchar(50) NOT NULL,
+  `Date` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
+  `id` int NOT NULL,
+  `post_id` int NOT NULL,
+  `admin_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `comment` varchar(1000) NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `likes`;
+CREATE TABLE `likes` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `admin_id` int NOT NULL,
+  `post_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE `posts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `admin_id` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `content` varchar(10000) NOT NULL,
+  `category` varchar(50) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `date` date DEFAULT NULL,
+  `status` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
+(1, 'Kush Tiwari ', 'test@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef');
+
+INSERT INTO `admin` (`id`, `name`, `password`) VALUES
+(1, 'admin', '6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2'),
+(2, 'rahul', '40bd001563085fc35165329ea1ff5c5ecbdbbeef');
